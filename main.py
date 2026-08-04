3c3bf68a77c7a48bbd9ca7944b53d084e28d60f0
Yu <ge75qeb@mytum.de>
2026-08-04 15:47:53 +0900

    feat: add main loop

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import time
 from camera import Camera
 from hand_tracker import HandTracker
 from gesture import GestureRecognizer
+from mouse_controller import MouseController
+import pyautogui
 
 
 
 
 if __name__ == "__main__":
+    print(f"size {pyautogui.size()}")
     hand_tracker = HandTracker()
     gesture = GestureRecognizer()
+    mouse_controller = MouseController(640, 480)
     cam = Camera()
     print("camera opened")
     while True:
@@ -21,9 +25,21 @@ if __name__ == "__main__":
        hand_tracker.find_hand(frame)
        frame = hand_tracker.draw_landmarks(frame)
        cv2.imshow("Trackpad", frame)
-       print(f"is left? {hand_tracker.is_left_hand()}")
-       print(gesture.fingers_up(hand_tracker.get_positions(frame), hand_tracker.is_left_hand()))
-       print(int(gesture.distance(hand_tracker.get_positions(frame), 4, 8)))
+      #  print(f"is left? {hand_tracker.is_left_hand()}")
+      #  print(gesture.fingers_up(hand_tracker.get_positions(frame), hand_tracker.is_left_hand()))
+      #  print(int(gesture.distance(hand_tracker.get_positions(frame), 4, 8)))
+       intent,pos = gesture.detect(hand_tracker.get_positions(frame), hand_tracker.is_left_hand())
+       if intent != "idle": 
+        print(intent)
+       if intent == "move":
+        mouse_controller.move(pos)
+       elif intent =="left_down":
+        mouse_controller.press("left")
+       elif intent =="left_up":
+        mouse_controller.release("left")
+       elif intent =="right":
+        mouse_controller.click("right")
+
        if cv2.waitKey(1) == ord('q'):
         break
     cam.release()
